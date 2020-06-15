0d11f6d61b53e8bd03797035c6b1563cd906f779
Szabo-Akos <szaboakos@outlook.com>
2020-06-15 18:55:25 +0200

    Fixed htaccess config

--- app/views/header.php
+++ app/views/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" type="text/css" href="../public/style/main.css">
+		<link rel="stylesheet" type="text/css" href="../style/main.css">
 		<title>Simple Payment</title>
